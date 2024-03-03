6ed2f4370e22f9c560d12af55048402e9fec598a
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-03-04 02:36:44 +0530

    done

--- UI/js/string.js
+++ UI/js/string.js
@@ -1,22 +1,54 @@
-//convert the Lower Case to Upper case 
+//convert the Lower Case to Upper case
 
-let str="aman sharma";
-let New_str ="";
-let lower ="abcdefghijklmnopqrstuvwxyz";
-let upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let str="aman sharma";
+// let New_str ="";
+// let lower ="abcdefghijklmnopqrstuvwxyz";
+// let upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-for(let i =0 ; i<=str.length-1 ; i++ ){
-    let char = str[i];
-    for(let j=0 ; j<=lower.length-1 ; j++){
-        if(char===lower[j]){
-            New_str = New_str + upper[j];
-        }
+// for(let i =0 ; i<=str.length-1 ; i++ ){
+//     let char = str[i];
+//     for(let j=0 ; j<=lower.length-1 ; j++){
+//         if(char===lower[j]){
+//             New_str = New_str + upper[j];
+//         }
+//     }
+// }
+
+// console.log(New_str);
+
+//convert the upper to lower case
+
+// let str = "AMAN SHARMA";
+// let New_str = "";
+// let lower = "abcdefghijklmnopqrstuvwxyz";
+// let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// for (let i = 0; i <= str.length - 1; i++) {
+//   let char = str[i];
+//   for (let j = 0; j <= upper.length - 1; j++) {
+//     if (char === upper[j]) {
+//       New_str = New_str + lower[j];
+//     }
+//   }
+// }
+
+// console.log(New_str);
+
+
+//CONVERT THE FIRST LETTER INTO UPPER CASE;
+
+
+let str="aman sharma is in dubai";
+let new_str ="";
+
+
+for(i=0 ; i<=str.length-1 ; i++){
+    // let new_Str ="";
+    if(i==0 || str[i-1]===" "){
+        new_str = new_str + str[i].toUpperCase() ;
+    }else {
+        new_str = new_str + str[i];
     }
 }
 
-console.log(New_str);
-
-
-
-
-//convert 
+console.log(new_str);
