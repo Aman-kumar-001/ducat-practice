9f88e244652eb59157f4e40f27dc4448c4504ebc
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-03-20 20:51:59 +0530

    js

---
+++ UI/js/array.js
@@ -0,0 +1,28 @@
+ //Q1 create a custom function in push method
+
+ let array = [10,20,30];
+ let push = [40,50,60,70,80]
+ let new_array = [];
+
+ 
+    for(j=0;j<=array.length -1;j++){
+        new_array = new_array +" "+ array[j];
+    }
+    for(i=0;i<=push.length-1;i++){
+        new_array = new_array + " " + push[i];
+    }
+
+    console.log(`Final Array we get is : ${new_array}`);
+
+   
+//Q2 create a custom function in pop method 
+
+let array1 = [100,200,300];
+let pop = [300];
+let new_array1 = []
+
+for(i=0 ; i<=array1.length-1;i++){
+    if(i==array1.length-1){
+       
+    }
+}
