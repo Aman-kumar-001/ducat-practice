c9dafdb915f5e2e9bbe9ff20d1dc724197c019cc
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-04-01 09:23:57 +0530

    object gone

--- UI/js/js-revision.js
+++ UI/js/js-revision.js
@@ -2,9 +2,38 @@
 
 
 let object ={
-    name : "Aman sharma",
-    age : "23"
+    name :{
+     firstName : "Aman",
+     lastNAme : "sharma"
+    } ,
+    age : "23",
+    getfullName : function(){
+        console.log("Getfullname is called");
+        console.log(this);
+    }
 }
-console.log(object);
+console.log(object , typeof object);
 console.log(object.name);
+
 console.log(object["name"]);
+console.log(object.getfullName());
+console.log(object["getfullName"]());
+console.log(Object.is(24 , 24));
+console.log(object.name?.firstName);
+
+
+//new object
+
+
+const obj = new Object({
+    name :{
+        firstName : "arjun",
+        lastNAme : "sharma"
+       } 
+})
+
+const obj2 = new Object();
+
+console.log(obj);
+console.log(obj2);
+
