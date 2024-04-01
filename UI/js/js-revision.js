36ca6fb2a3b4ef96ede8f05d0b1bf0d9c38a098b
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-04-01 23:14:09 +0530

    js question

--- UI/js/js-revision.js
+++ UI/js/js-revision.js
@@ -83,6 +83,103 @@ console.log(num2 , typeof num2);
 
 var randomNUm1 = Math.floor(Math.random()* 900000) + 100000;
 console.log(randomNUm1);
+console.log("question started")
+
+
+
+//Q1
+
+// let a=[];
+// let b=[];
+
+// console.log(a==b);
+// console.log(a===b);
+
+//answer :-- when ever we compare two array then its reference address get compare not the value.
+
+//Q2 
+
+// let a=[];
+// let b=a;
+
+// console.log(a==b);
+// console.log(a===b);
+
+//ans :-- ture , because both of them having the same address 
+
+
+//Q3
+
+let a2=[20];
+let b2 = [20];
+
+console.log(a2[0]== b2[0]);
+console.log(a2[0]===b2[0]);
+
+// here value get compare not the memmory
+
+//Q4 
+let z=[1,2,3,4]
+let a = {name : "anil"};
+console.log(...z);
+
+//spred operator is used here 
+
+//Q5 
+
+console.log(typeof NaN);
+
+//number 
+
+
+//Q6
+
+let data = 10 - - 10;
+console.log(data);
+
+// 20 bodmass rule
+
+//Q7
+
+const set = new Set([1,1,2,3,2,4]);
+console.log(set);
+
+// [1,2,3,4] because is take unique value
+
+//Q8
+
+let data2 = {name : "aman"};
+console.log(delete data2.name);
+
+//true
+
+//Q9 
+
+const data3 =["peter " , "aman" , "suraj"];
+const [x,y] =data3;
+console.log(x,y);
+
+// is asscess the data according to the index values
+
+//Q10
+
+const data4 =["peter " , "aman" , "suraj"];
+const [ ,q] =data3; //how to get the second value direct
+console.log(q);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
