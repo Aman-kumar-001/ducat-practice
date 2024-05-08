159db672e3f04099b7ce335485e77ea0953b2938
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-05-08 12:34:44 +0530

    chal rha hai

---
+++ UI/js/practice.js
@@ -0,0 +1,74 @@
+///scope or lexical enivironment
+
+function display(){
+    var name = "aman";
+    // console.log(name);
+}
+display();
+
+
+//IIFE
+(function() {
+    var message = "Hello, world!";
+    console.log(message); // Outputs: Hello, world!
+  })();
+
+
+//promise 
+
+const myPromise = new Promise ((resolve , reject ) =>{
+  setTimeout( ()=>{
+    console.log("hello")
+    resolve('operation done')
+  },2000);
+});
+
+myPromise.then((result) => console.log(result))
+.catch((err) => console.log(err));
+
+// CLASSES
+
+class person{
+
+  constructor(name , age){
+    this.name = name;
+    this.age = age;
+  }
+
+  displayName(){
+    console.log(`hello ${this.name} , your age is ${this.age} `);
+  }
+}
+
+
+const person1 = new person(aman,25);
+person1.displayName();
+
+//classes  with parent element
+
+class person{
+
+  constructor(name , age){
+    this.name = name;
+    this.age = age;
+  }
+
+  displayName(){
+    console.log(`hello ${this.name} , your age is ${this.age} `);
+  }
+}
+
+class student extends person{
+    constructor(name , age , group){
+        super(name , age);
+        this.group = group;
+    }
+   displayAll(){
+    console.log(`hello ${this.name} , your age is ${this.age} from ${this.group} `);
+  } 
+    
+}
+
+
+const person2 = new student( 'aman',25 , 'b.tech');
+person2.displayAll();
